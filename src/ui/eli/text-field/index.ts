8367f3b9bdeabb00a8ade3c9e9bf6967af040acb
lucka-me <samzhao0727@gmail.com>
2020-11-21 00:14:52 +0800

    Add and use eliTextField

---
+++ src/ui/eli/text-field/index.ts
@@ -0,0 +1,101 @@
+import { eli } from 'eli/eli';
+
+import './style.scss';
+
+const ClassName = {
+    textField: [
+        'mdc-text-field',
+        'mdc-text-field--outlined',
+    ].join(' '),
+    textfieldTextarea: [
+        'mdc-text-field--textarea',
+        'mdc-text-field--fullwidth'
+    ].join(' '),
+    textFieldWitchIcon: 'mdc-text-field--with-leading-icon',
+    textFieldIcon: [
+        'fa',
+        'mdc-text-field__icon',
+        'mdc-text-field__icon--leading'
+    ].join(' '),
+    input: 'mdc-text-field__input',
+    outline: 'mdc-notched-outline',
+    outlineLeading: 'mdc-notched-outline__leading',
+    outlineNotch: 'mdc-notched-outline__notch',
+    label: 'mdc-floating-label',
+    outlineTrailing: 'mdc-notched-outline__trailing',
+
+    helper: 'mdc-text-field-helper-line',
+    helperText: 'mdc-text-field-helper-text mdc-text-field-helper-text--persistent',
+};
+
+interface EliTextFieldOptions {
+    id: string,
+    label: string,
+    textarea?: boolean,
+    icon?: string,
+    inputType?: string,
+    inputMode?: string,
+    pattern?: string,
+    required?: boolean,
+}
+
+export function eliTextField(options: EliTextFieldOptions): HTMLDivElement {
+    let classNameRoot = ClassName.textField;
+    const contents: Array<HTMLElement> = [
+        eli('div', {
+            className: ClassName.outline,
+        }, [
+            eli('div', { className: ClassName.outlineLeading }),
+            eli('div', {
+                className: ClassName.outlineNotch,
+            }, [
+                eli('span', {
+                    className: ClassName.label,
+                    id: options.id,
+                    innerHTML: options.label,
+                }),
+            ]),
+            eli('div', { className: ClassName.outlineTrailing }),
+        ])
+    ];
+    if (options.textarea) {
+        classNameRoot += ` ${ClassName.textfieldTextarea}`;
+        contents.push(eli('textarea', {
+            className: ClassName.input,
+            id: options.id,
+            rows: 8, cols: 80
+        }));
+    } else {
+        if (options.icon) {
+            classNameRoot += ` ${ClassName.textFieldWitchIcon}`;
+            contents.push(eli('i', {
+                className: ClassName.textFieldIcon,
+                innerHTML: options.icon,
+            }));
+        }
+        const elementInput = eli('input', {
+            className: ClassName.input,
+            ariaLabelledby: options.id,
+        });
+        if (options.inputType) elementInput.type = options.inputType;
+        if (options.inputMode) elementInput.inputMode = options.inputMode;
+        if (options.pattern) elementInput.pattern = options.pattern;
+        if ('required' in options) elementInput.required = options.required;
+
+        contents.push(elementInput);
+    }
+    
+    return eli('div', { className: classNameRoot, }, contents);
+}
+
+export namespace eliTextField {
+    export function helper(contents: Array<HTMLElement>) {
+        return eli('div', {
+            className: ClassName.helper,
+        }, [
+            eli('div', {
+                className: ClassName.helperText,
+            }, contents),
+        ]);
+    }
+}
--- src/ui/panel/index.ts
+++ src/ui/panel/index.ts
@@ -8,6 +8,7 @@ import { eli } from 'eli/eli';
 import { eliButton } from 'eli/button';
 import { eliDialog } from 'eli/dialog';
 import { eliSwitch } from 'eli/switch';
+import { eliTextField } from 'eli/text-field';
 import { service } from 'service';
 import { version } from 'root/package.json';
 
@@ -63,74 +64,93 @@ export default class Panel extends base.Prototype {
         // Contents
         const contents: Array<HTMLElement> = [];
 
-        // Preference: Camera
+        // Panel: Camera
         contents.push(eli('span', { className: 'headline', innerHTML: 'Camera' }));
-        const contentsCamera: Array<HTMLElement> = [];
-        // Preference: Camera - Location
-        const elementCameraLongitude = this.buildTextfield('text', 'decimal', 'Longitude', 'input-pref-camera-lon');
-        this.items.camera.longitude = new MDCTextField(elementCameraLongitude);
-        this.items.camera.longitude.value = service.preference.get('mapler.camera.lon');
+        // Panel: Camera - Location
+        const elementCameraLongitude = eliTextField({
+            id: 'panel-camera-lon',
+            label: 'Longitude',
+            inputType: 'text',
+            inputMode: 'decimal',
+            required: true,
+        });
+        const elementCameraLatitude = eliTextField({
+            id: 'panel-camera-lat',
+            label: 'Latitude',
+            inputType: 'text',
+            inputMode: 'decimal',
+            required: true,
+        });
+        // Panel: Camera - Others
+        const elementCameraZoom = eliTextField({
+            id: 'panel-camera-zoom',
+            label: 'Zoom',
+            inputType: 'text',
+            inputMode: 'decimal',
+            required: true,
+        });
+        const elementCameraBearing = eliTextField({
+            id: 'panel-camera-bearing',
+            label: 'Bearing',
+            inputType: 'text',
+            inputMode: 'decimal',
+            required: true,
+        });
+        const elementCameraTilt = eliTextField({
+            id: 'panel-camera-tilt',
+            label: 'Tilt',
+            inputType: 'text',
+            inputMode: 'decimal',
+            required: true,
+        });
+        // Panel: Camera - Set
+        const elementSet = eliButton('Set');
 
-        const elementCameraLatitude = this.buildTextfield('text', 'decimal', 'Latitude ', 'input-pref-camera-lat');
-        this.items.camera.latitude = new MDCTextField(elementCameraLatitude);
-        this.items.camera.latitude.value = service.preference.get('mapler.camera.lat');
-
-        contentsCamera.push(eli('div', {
-            className: 'location'
-        }, [
-            elementCameraLongitude, elementCameraLatitude
+        // Build Panel: Camera
+        contents.push(eli('div', { className: 'camera' }, [
+            eli('div', {
+                className: 'location'
+            }, [
+                elementCameraLongitude, elementCameraLatitude
+            ]),
+            elementCameraZoom, elementCameraBearing, elementCameraTilt,
+            elementSet
         ]));
 
-        // Preference: Camera - Others
-        const elementCameraZoom = this.buildTextfield('text', 'decimal', 'Zoom', 'input-pref-camera-zoom');
-        this.items.camera.zoom = new MDCTextField(elementCameraZoom);
-        this.items.camera.zoom.value = service.preference.get('mapler.camera.zoom');
-
-        const elementCameraBearing = this.buildTextfield('text', 'decimal', 'Bearing', 'input-pref-camera-bearing');
-        this.items.camera.bearing = new MDCTextField(elementCameraBearing);
-        this.items.camera.bearing.value = service.preference.get('mapler.camera.bearing');
-
-        const elementCameraTilt = this.buildTextfield('text', 'decimal', 'Tilt', 'input-pref-camera-tilt');
-        this.items.camera.tilt = new MDCTextField(elementCameraTilt);
-        this.items.camera.tilt.value = service.preference.get('mapler.camera.tilt');
-
-        contentsCamera.push(
-            elementCameraZoom, elementCameraBearing, elementCameraTilt
-        );
-
-        // Preference: Camera - Set
-        const elementSet = eliButton('Set');
-        contentsCamera.push(elementSet);
-
-        // Build Preference: Camera
-        contents.push(eli('div', { className: 'camera' }, contentsCamera));
-
-        // Preference: Size
+        // Panel: Size
         contents.push(eli('span', { className: 'headline', innerHTML: 'Size' }));
-        const pixelRatio = window.devicePixelRatio;
 
-        // Preference: Size - Width
-        const elementSizeWidth = this.buildTextfield('number', 'numeric', 'Width', 'input-pref-size-width');
-        
-        this.items.size.width = new MDCTextField(elementSizeWidth);
-        this.items.size.width.value = `${window.screen.width * pixelRatio}`;
-
-        // Preference: Size - Height
-        const elementSizeHeight = this.buildTextfield('number', 'numeric', 'Height', 'input-pref-size-height');
-        this.items.size.height = new MDCTextField(elementSizeHeight);
-        this.items.size.height.value = `${window.screen.height * pixelRatio}`;
-
-        // Preference: Size - Pixel Ratio
-        const elementSizePixelRatio = this.buildTextfield('number', 'numeric', 'Pixel Ratio', 'input-pref-size-pixelRatio');
-        this.items.size.pixelRatio = new MDCTextField(elementSizePixelRatio);
-        this.items.size.pixelRatio.value = `${pixelRatio}`;
+        // Panel: Size - Width
+        const elementSizeWidth = eliTextField({
+            id: 'panel-size-width',
+            label: 'Width',
+            inputType: 'number',
+            inputMode: 'numeric',
+            required: true,
+        });
+        // Panel: Size - Height
+        const elementSizeHeight = eliTextField({
+            id: 'panel-size-height',
+            label: 'Height',
+            inputType: 'number',
+            inputMode: 'numeric',
+            required: true,
+        });
+        // Panel: Size - Pixel Ratio
+        const elementSizePixelRatio = eliTextField({
+            id: 'panel-size-pixelRatio',
+            label: 'Pixel Ratio',
+            inputType: 'number',
+            inputMode: 'numeric',
+            required: true,
+        });
 
         contents.push(elementSizeWidth, elementSizeHeight, elementSizePixelRatio);
 
-        // Preference: Display
+        // Panel: Display
         contents.push(eli('span', { className: 'headline', innerHTML: 'Display' }));
 
-        // Preference: Display - Labels
+        // Panel: Display - Labels
         const elementDisplayLabels = eliSwitch('input-panel-display-labels');
         contents.push(eli('div', {
             className: 'switch',
@@ -168,8 +188,26 @@ export default class Panel extends base.Prototype {
         this.parent.append(elementDialog);
         this.ctrl = new MDCDialog(elementDialog);
 
+        this.items.camera.longitude = new MDCTextField(elementCameraLongitude);
+        this.items.camera.longitude.value = service.preference.get('mapler.camera.lon');
+        this.items.camera.latitude = new MDCTextField(elementCameraLatitude);
+        this.items.camera.latitude.value = service.preference.get('mapler.camera.lat');
+        this.items.camera.zoom = new MDCTextField(elementCameraZoom);
+        this.items.camera.zoom.value = service.preference.get('mapler.camera.zoom');
+        this.items.camera.bearing = new MDCTextField(elementCameraBearing);
+        this.items.camera.bearing.value = service.preference.get('mapler.camera.bearing');
+        this.items.camera.tilt = new MDCTextField(elementCameraTilt);
+        this.items.camera.tilt.value = service.preference.get('mapler.camera.tilt');
         const rippleSet = new MDCRipple(elementSet);
         rippleSet.listen('click', () => this.onSetCamera());
+
+        const pixelRatio = window.devicePixelRatio;
+        this.items.size.width = new MDCTextField(elementSizeWidth);
+        this.items.size.width.value = `${window.screen.width * pixelRatio}`;
+        this.items.size.height = new MDCTextField(elementSizeHeight);
+        this.items.size.height.value = `${window.screen.height * pixelRatio}`;
+        this.items.size.pixelRatio = new MDCTextField(elementSizePixelRatio);
+        this.items.size.pixelRatio.value = `${pixelRatio}`;
 
         this.items.display.labels = new MDCSwitch(elementDisplayLabels);
         this.items.display.labels.checked = service.preference.get('mapler.display.labels');
@@ -286,34 +324,5 @@ export default class Panel extends base.Prototype {
                 tilt: tilt
             });
         }
-    }
-
-    /**
-     * Build a MDC textfield element
-     * @param type Type of the `<input>`, like `text` or `number`
-     * @param inputmode Input mode of the `<input>`, like `decimal` or `number`
-     * @param label Text displayed in label
-     * @param id Id for `<input>` and `<label>`
-     * @returns The headline element
-     */
-    private buildTextfield(type: string, inputmode: string, label: string, id: string): HTMLDivElement {
-        return eli('div', {
-            className: 'mdc-text-field mdc-text-field--outlined margin-v--8 margin-h--4'
-        }, [
-            eli('input', {
-                type: type, inputmode: inputmode, id: id,
-                className: 'mdc-text-field__input'
-            }),
-            eli('div', { className: 'mdc-notched-outline' }, [
-                eli('div', { className: 'mdc-notched-outline__leading' }),
-                eli('div', { className: 'mdc-notched-outline__notch' }, [
-                    eli('label', {
-                        for: id, innerHTML: label,
-                        className: 'mdc-floating-label'
-                    })
-                ]),
-                eli('div', { className: 'mdc-notched-outline__trailing' }),
-            ])
-        ]);
     }
 }
